d076c754864bd91d44e00b212139190ae98a97c7
Jessica Leigh <jleigh@pietech.com>
2017-12-10 21:46:27 -0500

    created folders for js and css files.  Initiated firebase, created html layout, added jumbotron styling

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDx5FV9rivnNtKuC_mNBVndqw10Jb282LI",
+    authDomain: "train-time-6988f.firebaseapp.com",
+    databaseURL: "https://train-time-6988f.firebaseio.com",
+    projectId: "train-time-6988f",
+    storageBucket: "train-time-6988f.appspot.com",
+    messagingSenderId: "276002555880"
+  };
+  firebase.initializeApp(config);
